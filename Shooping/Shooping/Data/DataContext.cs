5a5e52347a6dbc3a1ddacfca8b48d50fc8267bd0
Valdom <valdom33@gmail.com>
2022-03-18 21:59:06 +0100

    Empezar provincias CRUD

--- Shooping/Shooping/Data/DataContext.cs
+++ Shooping/Shooping/Data/DataContext.cs
@@ -10,14 +10,19 @@ namespace Shooping.Data
 
         }
 
+
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Country> Countries{ get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<State> States{ get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
+            modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique(); //Expresion landa. El VS autocompreta
             modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+            modelBuilder.Entity<State>().HasIndex("Name", "CountryId").IsUnique(); //Con nombre de campo. Aquí no autocompleta 
+            modelBuilder.Entity<City>().HasIndex("Name", "StateId").IsUnique();
         }
     }
 }
